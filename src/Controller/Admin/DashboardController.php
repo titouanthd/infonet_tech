18b6c36128dfd76bba734aaec023b4e84ea80d61
titouanthd <titouan.thd@gmail.com>
2023-03-22 06:49:40 +0100

    feat: add counter on dashboard for movies and characters

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\CharacterRepository;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
-    public function index(): Response
+    public function index(CharacterRepository $characterRepository, MovieRepository $movieRepository): Response
     {
+        $countMovies = $movieRepository->count([]);
+        $countCharacters = $characterRepository->count([]);
+        
         return $this->render('admin/dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
             'page_title' => 'Admin Dashboard',
+            'count_movies' => $countMovies,
+            'count_characters' => $countCharacters,
         ]);
     }
 }
